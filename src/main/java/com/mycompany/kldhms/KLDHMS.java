3a06aec6e85f52d6e240018d4f2b7eafbb645bb8
sceed <6ckgarcia@gmail.com>
2025-04-25 07:11:23 +0800

    Initial Commit

---
+++ src/main/java/com/mycompany/kldhms/KLDHMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kldhms;
+
+/**
+ *
+ * @author 6scee
+ */
+public class KLDHMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
